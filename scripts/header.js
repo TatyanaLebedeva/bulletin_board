252350f5ecc0d1f08083a7687a22b37289b993e2
TatyanaLebedeva <5BK9BD5KcQw@srR>
2023-12-25 22:53:10 +0300

    add page.product.js

--- scripts/header.js
+++ scripts/header.js
@@ -1,8 +1,30 @@
 (function (app) {
     app.Header = {
         draw: function () {
+            let name=document.createElement("p");
+            name.append(document.createTextNode("Объявление.RU"));
+
+            let formTape = document.createElement("p");
+            formTape.append(document.createTextNode("Лента"));
+            // formTape.classList.add(".form_header_text");
+
+            let formAd = document.createElement("p");
+            formAd.append(document.createTextNode("Мои объявления"));
+            // formAd.classList.add(".form_header_text");
+
+            let formExit = document.createElement("p");
+            formExit.append(document.createTextNode("Выход"));
+            // formExit.classList.add(".form_header_text");
+
+            let rightBlockHeader = document.querySelector(".right_block_header");
+            rightBlockHeader.append(formTape, formAd, formExit);
+
+
+            let block = document.querySelector(".position_header");
+            block.append(name, rightBlockHeader);
+
             document.querySelector("header")
-                .append(document.createTextNode("Объявление.RU"));
+                .append(block);
         }
     }
 })(ADSBoard);
---
+++ scripts/page.product.js
@@ -0,0 +1,53 @@
+(function (app) {
+    app.PageProduct = {
+        draw: function () {
+            let image = document.createElement('p');
+            image.className = 'product__image';
+            let phone = document.createElement('p');
+            phone.className = 'product__phone';
+            let phoneButton = document.createElement("button");
+            phoneButton.classList.add("phone_button");
+            phoneButton.append(document.createTextNode("Показать телефон"));
+
+
+
+
+            let title = document.createElement('p');
+            title.append(document.createTextNode("Планшет Galaxy tab"));
+            title.className = 'product__title';
+            let about = document.createElement('p');
+            about.append(document.createTextNode("Немного потрепанный, цвет черный"));
+            about.className = 'product__about';
+            let seller = document.createElement('p');
+            seller.append(document.createTextNode("Продавец: Сосорев О.А."));
+            seller.className = 'product__seller';
+
+            let sum = document.createElement('p');
+            sum.append(document.createTextNode("3000 p"));
+            sum.className = 'product__sum';
+
+            let leftBlock = document.querySelector(".left_block");
+            leftBlock.append(image, phone, phoneButton);
+
+            phoneButton.addEventListener("click", showPhone);
+
+            let centerBlock = document.querySelector(".center_block");
+            centerBlock.append(title, about, seller);
+
+            let rightBlock = document.querySelector(".right_block");
+            rightBlock.append(sum);
+
+            let productBlock = document.querySelector(".product_block");
+            productBlock.append(leftBlock, centerBlock, rightBlock);
+
+            let productContent = document.querySelector(".product_content");
+            productContent.append(productBlock);
+        }
+    }
+    function showPhone() {
+        document.querySelector(".phone_button").remove();
+        let leftBlock=document.querySelector(".left_block");
+        leftBlock.append("+7 968 562 32 52");
+    }
+
+})(ADSBoard);
